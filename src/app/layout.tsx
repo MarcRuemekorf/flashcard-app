ff0075c8107b244e1987767b4fa79cfd1f18c54d
Marc Ruemekorf <marc.ruemekorf@zilliz.nl>
2025-10-31 17:20:07 +0100

    Move components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import Providers from '@/components/providers'
+import Providers from '@/components/shared/providers'
 
 export const metadata: Metadata = {
   title: 'Flashcard App',
